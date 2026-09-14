b8bc7e800c00502e796baf56acfc09b782845ada
nic | nic.ethkl.eth <nicholas@etherscan.io>
2026-09-14 22:18:05 +0800

    Manual bump

--- Formula/etherscan.rb
+++ Formula/etherscan.rb
@@ -4,30 +4,30 @@
 class Etherscan < Formula
   desc "Command-line client and interactive explorer for the Etherscan V2 API"
   homepage "https://github.com/etherscan/etherscan-cli"
-  version "1.0.8"
+  version "1.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.0.8/etherscan_1.0.8_darwin_amd64.tar.gz"
-      sha256 "adf19503884a240e3256703ab1b2c5efe135aa63f78d7afab041b8685e17c5ae"
+      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.1.1/etherscan_1.1.1_darwin_amd64.tar.gz"
+      sha256 "e48101707e765307d3529ca3d239290bcae45b013a61ce14ace2b9a95ece8603"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.0.8/etherscan_1.0.8_darwin_arm64.tar.gz"
-      sha256 "2438ea7f591188edcd00d7aca178aad7e075b1129b3a7701d351b9445774dc1c"
+      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.1.1/etherscan_1.1.1_darwin_arm64.tar.gz"
+      sha256 "d695f6397345e803c67148c62146a9ef8ee5075edffbe1e0031cd8272f20d602"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.0.8/etherscan_1.0.8_linux_amd64.tar.gz"
-      sha256 "68b52966b1bee9479a399765d1ba7e8bae240e71027a63d46095b7f3fc3dc992"
+      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.1.1/etherscan_1.1.1_linux_amd64.tar.gz"
+      sha256 "33b5ae2bb688cc1aef4e97a41b87055013ed492aaf1651e66e3cd43aae24ee0a"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.0.8/etherscan_1.0.8_linux_arm64.tar.gz"
-      sha256 "e025705c7b20a8ee560dba36b243bcac4892b026335ef6fe78e470f04da3752a"
+      url "https://github.com/etherscan/etherscan-cli/releases/download/v1.1.1/etherscan_1.1.1_linux_arm64.tar.gz"
+      sha256 "4918a2e5c7f477705be72f5b1878d6e7b9422efd02e243fb0643797cfe2cb366"
     end
   end
 
